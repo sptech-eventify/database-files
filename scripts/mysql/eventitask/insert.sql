19296355c84893fae88f2ca83e819e20351905b5
LeoVasc5 <leovasc5@hotmail.com>
2023-09-17 01:47:48 -0300

    Inserindo notebook

--- scripts/mysql/eventitask/insert.sql
+++ scripts/mysql/eventitask/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO usuario (name, email, password, img) VALUES 
+INSERT INTO eventitask.usuario (name, email, password, img) VALUES 
     ('Brayan Coelho', 'brayan.aquino@sptech.school','$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://i.kym-cdn.com/photos/images/newsfeed/002/440/418/06a'),
 	('Guilherme Henrique', 'guilherme.dias@sptech.school', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://avatars.githubusercontent.com/u/99812860?v=4'),
 	('Helder Davidson', 'helder.alvarenga@sptech.school', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e',  'https://avatars.githubusercontent.com/u/70406114?v=4'),
@@ -6,17 +6,17 @@ INSERT INTO usuario (name, email, password, img) VALUES
     ('Victor Hugo', 'victor.nascimento@sptech.school', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://avatars.githubusercontent.com/u/99813230?v=4'),
 	('Vitor Gonçalves', 'vitor@gmail.com', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://avatars.githubusercontent.com/u/70069239?v=4');
 
-INSERT INTO board (name) VALUES 
+INSERT INTO eventitask.board (name) VALUES 
 	("Front-end"), 
     ("Back-end"),
     ("Teórico e Outros");
 
-INSERT INTO section (board_id, name, color) VALUES
-	(1, "Sprint 1", "bg-emerald-400"),
-    (2, "Sprint 1", "bg-emerald-400"),
-    (3, "Sprint 1", "bg-emerald-400");
+INSERT INTO eventitask.section (board_id, name, color) VALUES
+	(1, "Sprint IV", "bg-emerald-400"),
+    (2, "Sprint IV", "bg-emerald-400"),
+    (3, "Sprint IV", "bg-emerald-400");
     
-INSERT INTO task (section_id, name, description, priority, fibonacci, status, time, data_estimada) VALUES 
+INSERT INTO eventitask.task (section_id, name, description, priority, fibonacci, status, time, data_estimada) VALUES 
 	(1, "Tela de Login", "Tela de login para autenticar o usuario atraves do jwt.", 0, 3, 0, 2, DATE(CURRENT_TIMESTAMP())),
 	(1, "JWT", "Autenticação.", 0, 3, 0, 4, DATE(CURRENT_TIMESTAMP())),
     (2, "Componente de Buffet", "Criar componente de buffet estilizado para visualização pública.", 0, 3, 0, 8, DATE(CURRENT_TIMESTAMP())),
@@ -24,7 +24,7 @@ INSERT INTO task (section_id, name, description, priority, fibonacci, status, ti
     (3, "Documentação", "Reformatar, adaptar e desenvolver novos textos para o embasamento do projeto.", 0, 3, 0, 5, DATE(CURRENT_TIMESTAMP())),
 	(3, "Modelagem", "Entregar o diagrama de banco de dados de acordo com o escopo fechado do projeto.", 0, 3 ,0, 4, DATE(CURRENT_TIMESTAMP()));
 
-INSERT INTO user_task VALUES
+INSERT INTO eventitask.user_task VALUES
 	(1, 1),
     (2, 2),
 	(1, 2),
@@ -35,10 +35,10 @@ INSERT INTO user_task VALUES
     (5, 5),
     (6, 6);
 
-INSERT INTO tag (name) VALUES 
+INSERT INTO eventitask.tag (name) VALUES 
 	("front end"),
 	("back end"),
     ("qa"),
     ("devops");
 
-UPDATE task SET STATUS = 1 WHERE id = 2;
+UPDATE eventitask.task SET STATUS = 1 WHERE id = 2;
