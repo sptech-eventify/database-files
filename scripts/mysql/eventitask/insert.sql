6a88cea34951e2ac90e90a20de6662507cc2e5fc
LeoVasc5 <leovasc5@hotmail.com>
2023-09-18 17:55:27 -0300

    Inserido foto do Brayan

--- scripts/mysql/eventitask/insert.sql
+++ scripts/mysql/eventitask/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO eventitask.user (name, email, password, img) VALUES 
-    ('Brayan Coelho', 'brayan.aquino@sptech.school','$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://i.kym-cdn.com/photos/images/newsfeed/002/440/418/06a'),
+    ('Brayan Coelho', 'brayan.aquino@sptech.school','$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://avatars.githubusercontent.com/u/99812492?v=4'),
 	('Guilherme Henrique', 'guilherme.dias@sptech.school', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://avatars.githubusercontent.com/u/99812860?v=4'),
 	('Helder Davidson', 'helder.alvarenga@sptech.school', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e',  'https://avatars.githubusercontent.com/u/70406114?v=4'),
     ('Leonardo Vasconcelos', 'leonardo.paulino@sptech.school', '$2b$10$LC/5qzu7vlhsHGxlHOCZ7uznDtgBgXDlUCBLIn.3EWn1kksAxTs9e', 'https://avatars.githubusercontent.com/u/70069239?v=4'),
