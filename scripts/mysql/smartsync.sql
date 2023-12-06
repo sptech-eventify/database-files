005151f0c155dab94c89eb2da324331cf9a3c35b
LeoVasc5 <leovasc5@hotmail.com>
2023-12-06 15:32:48 -0300

    Novos gráficos

--- scripts/mysql/smartsync.sql
+++ scripts/mysql/smartsync.sql
@@ -1,16 +1,41 @@
 use eventify;
 
-SELECT * FROM buffet;
+CREATE OR REPLACE VIEW vw_faturamento_buffet AS (
+SELECT 
+	b.id, 
+    SUM(e.preco) faturamento_evento
+FROM
+	buffet b
+JOIN
+	evento e ON e.id_buffet = b.id
+WHERE
+	e.status = 6
+GROUP BY b.id ORDER BY id
+);
 
 
-SELECT b.id, SUM(preco) FROM evento e join buffet b on b.id = e.id_buffet
-WHERE e.status = 6
-GROUP BY b.id;
+CREATE OR REPLACE VIEW vw_gastos_buffet AS (
+SELECT 
+	b.id, 
+    SUM(t.valor) gasto_evento
+FROM
+	buffet b
+JOIN
+	transacao t ON t.id_buffet = b.id
+GROUP BY b.id ORDER BY id);
 
-SELECT b.id, SUM(valor) FROM transacao t join buffet b on b.id = t.id_buffet
-WHERE t.is_gasto = 1
-GROUP BY b.id;
 
 
-SELECT * FROM transacao;
 
+
+
+    
+
+-- buffetsSemVisitas (15) (lista)
+-- taxa conversao visitas em fracao escala  (calculo back)
+-- categorias (lista categoria, qtd)
+-- taxa contratos cancelados (calculo back)
+
+-- grafico de barras de todos eventos 
+-- clientes que fecharam 0 / 1 / 2 / 3 ou mais (calculo back, retorna lista)
+-- Uso do formulário dinâmico
